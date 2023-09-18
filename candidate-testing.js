09e08ca1659aacadeea4574e6eed06575bf0b74b
Nagappa, Arpitha <arpithagn021@gmail.com>
2023-09-17 23:07:13 -0500

    part2

--- candidate-testing.js
+++ candidate-testing.js
@@ -13,8 +13,9 @@ let candidateAnswer= "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
+let questions =["Who was the first American woman in space? ","True or false: 5 kilometer == 5000 meters? ", 
+"(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true" , "40", "Trajectory", "3"];
 let candidateAnswers;
 
 
@@ -27,18 +28,29 @@ console.log("CandidateName:" +candidateName);
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-let candidateAnswer = input.question("Who was the first American woman in space? ");
-candidateAnswer = console.log(candidateAnswer);
+//let candidateAnswer = input.question("Who was the first American woman in space? ");
+//candidateAnswer = console.log(candidateAnswer);
+for(i=0;i<questions.length-1;i++){
+  candidateAnswers[i] = input.question(questions[i]);
+}
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if(candidateAnswer=== correctAnswer){
+/*if(candidateAnswer=== correctAnswer){
   console.log("Your answer is correct");
 }else{
   console.log("Your answer is not correct");
+}*/
+
+for(i=0;i<questions.length-1;i++){
+  console.log(`${i+1}) ${questions[i]}`);
+  console.log(`Your Answer: ${candidateAnswers[i]}`);
+  console.log(`Correct Answer: ${correctAnswers[i]}`);
 }
+
+
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
